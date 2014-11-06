ba7c38f1933f38e916752136181e129b496e40e9
MohamedOs <mos4ma@gmail.com>
2014-11-06 15:08:40 +0200

    Update 7.js

--- qa/7.js
+++ qa/7.js
@@ -1,25 +1,68 @@
-var arr = [];
-var vars={};
-function pri(priv,pageNumber){
-vars.city=priv;
-var xhr = Titanium.Network.createHTTPClient();
-xhr.onload = function () {
-var select = soupselect.select;
-var body=xhr.responseText;
-var handler = new htmlparser.DefaultHandler(function(err, dom) {
-	if (err) {
-		alert('Error: ' + err);
-	} else {
-		}
-	}
+var arr7 = [];
+var vars7={};
+function pri7(priv,pageNumber){
+vars7.city=priv;
+var queryIndexF7='https://mobile.twitter.com/search?q=tahrirnews';
+Titanium.Yahoo.yql(queryIndexF7, function(e){
+vars7.data=e.data;
+function getObjects(obj, key, val) {
+var objects = [];
+for (var i in obj) {
+if (!obj.hasOwnProperty(i)) continue;
+if (typeof obj[i] == 'object') {
+objects = objects.concat(getObjects(obj[i], key, val));
+} else
+//if key matches and value matches or if key matches and value is not passed (eliminating the case where key matches but passed value does not)
+if (i == key && obj[i] == val || i == key && val == '') { //
+objects.push(obj);
+} else if (obj[i] == val && key == ''){
+//only add if the object is not already in the array
+if (objects.lastIndexOf(obj) == -1){
+objects.push(obj);
+}
+}
+}
+return objects;
+}
+//return an array of values that match on a certain key
+function getValues(obj, key) {
+var objects = [];
+for (var i in obj) {
+if (!obj.hasOwnProperty(i)) continue;
+if (typeof obj[i] == 'object') {
+objects = objects.concat(getValues(obj[i], key));
+} else if (i == key) {
+objects.push(obj[i]);
+}
+}
+return objects;
+}
+//return an array of keys that match on a certain value
+function getKeys(obj, val) {
+var objects = [];
+for (var i in obj) {
+if (!obj.hasOwnProperty(i)) continue;
+if (typeof obj[i] == 'object') {
+objects = objects.concat(getKeys(obj[i], val));
+} else if (obj[i] == val) {
+objects.push(i);
+}
+}
+return objects;
+}
+var ds7 = vars7.data;
+vars7.d1=(getObjects(ds7,'class','avatar'));
+vars7.d2=(getObjects(ds7,'class','tweet-text'));
+for(var i=0;i<vars7.d2.length;i++){
+arr7.push({
+image:vars7.d1[0].a.img.src,
+title:vars7.d1[0].a.img.alt,
+content:vars7.d2[0].div.p.content,
+});	
+}
+
+}
+Ti.API.info(arr7);
+
 });
-var parser = new htmlparser.Parser(handler);
-parser.parseComplete(body);
-};
-xhr.onerror = function() {
-    Titanium.API.info('error');
-};
-var url="https://mobile.twitter.com/search?q=tahrirnews&s=hash";
-xhr.open("GET",url);
-xhr.send();
 }
