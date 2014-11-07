1d06afe52c4f3215210a7ff6f619583d68520550
MohamedOs <mos4ma@gmail.com>
2014-11-07 14:35:05 +0200

    Update 4.js

--- qa/4.js
+++ qa/4.js
@@ -1,7 +1,7 @@
 var arr4 = [];
 var vars4={};
 function pri4(priv7,pageNumber){
-var url="http://qatar.dubizzle.com"+priv7+pageNumber;
+var url="http://qatar.dubizzle.com"+priv7+"?page="+pageNumber;
 //activityIndicatorCatt.show();
 vars4.city=priv7;
 var xhr = Titanium.Network.createHTTPClient();
