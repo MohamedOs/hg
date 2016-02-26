e3dc02654c4344019735d3a2281217da9992d961
MohamedOs <mos4ma@gmail.com>
2016-02-26 10:30:03 +0200

    Update 1.js

--- Sa/1.js
+++ Sa/1.js
@@ -30,9 +30,92 @@ borderRadius:7
 });*/
 var arr = [];
 var vars={};
-var carsFor=[{"title":"title,"href":"href"},{"title":"title,"href":"href"}]
-
 function pri(priv,pageNumber){
-//activityIndicatorCatt.hide();
-arr=[{"image":"https://olxsaimages-a.akamaihd.net/f5da4fcf757c38f3d7f54ecdf29f8049/olxsa_47343_1_261x203.jpg?rev=002,"title":"title,"href":"http://goo.com,"price":"22","place":"place","date","date"}];	
+	
+//activityIndicatorCatt.show();
+vars.city=priv;
+
+var xhr = Titanium.Network.createHTTPClient();
+xhr.onload = function () {
+	 
+var select = soupselect.select;
+
+var body=xhr.responseText;
+var handler = new htmlparser.DefaultHandler(function(err, dom) {
+	if (err) {
+		alert('Error: ' + err);
+	} else {
+		vars.data=select(dom,'a.d-listing__name');
+		var ds = vars.data;
+		var photos=select(dom,'img.u-img__img');
+		//Ti.API.info(photos[0].attribs.src);
+		var titles=select(dom,'a.d-listing__name');
+		//Ti.API.info(titles[0].children[0].data);
+		var prices=select(dom,'div.d-listing__amount');
+		//Ti.API.info(prices[0].children[0].data);
+		var hrefs=select(dom,'a.d-listing__name');
+		//Ti.API.info(hrefs[0].attribs.href);
+		var places=select(dom,'div.d-listing__meta');
+		//Ti.API.info(places[0].children[1].children[0].data+' - '+places[0].children[3].children[0].data);
+		var dates=select(dom,'span.d-listing__date');
+		//Ti.API.info(dates[0].children[0].data);
+		
+		for (var i=0;i<titles.length;i++) {
+		var imageVar,priceVar,titleVar,hrefVar,placeVar,dateVar;
+		titleVar=titles[i].children[0].data;
+		titleVar=titleVar.trim();
+		hrefVar=hrefs[i].attribs.href;
+		hrefVar=hrefVar.trim();
+		if(photos[i].attribs){
+		imageVar=photos[i].attribs.src;	
+		imageVar=imageVar.trim();
+		}else{
+		imageVar="photos[0].attribs.src";
+		imageVar=imageVar.trim();
+		}
+		if(prices[i]){
+		priceVar=prices[i].children[0].data;	
+		priceVar=priceVar.trim();
+		}else{
+		priceVar="-";	
+		priceVar=priceVar.trim();
+		}
+		placeVar=places[i].children[1].children[0].data+' - '+places[i].children[3].children[0].data;
+		placeVar=placeVar.trim();
+		dateVar=dates[i].children[0].data;
+		dateVar=dateVar.trim();
+		 arr.push({
+                  image:imageVar,
+                  title:titleVar,
+                  href:hrefVar,
+                  price:priceVar,
+                  place:placeVar,
+                  date:dateVar
+                });	
+	//		
+		}
+		Ti.API.info(arr);
+		// activityIndicatorCatt.hide();
+	
+	}
+});
+ 
+var parser = new htmlparser.Parser(handler);
+parser.parseComplete(body);
+
+
+
+
+
+   // Titanium.API.info(xhr.responseText);
+};
+xhr.onerror = function() {
+    Titanium.API.info('error');
+};
+var url="https://olx.com.eg/ar/dakahlia/search/";
+xhr.open("GET",url);
+xhr.send();
+
+
+
 }
