b3fccf0633b8dcd903fdee97366c46180449c49c
MohamedOs <mos4ma@gmail.com>
2016-02-26 10:54:00 +0200

    Added files via upload

--- All/1.js
+++ All/1.js
@@ -30,7 +30,9 @@ borderRadius:7
 });*/
 var arr = [];
 var vars={};
-function pri(nation,priv,pageNumber){	
+
+function pri(priv,pageNumber){
+alert("نعتذر جدا! التطبيق الان فى حالة صيانه ومن الممكن عرض اعلانات قديمه جاري حل بعض المشاكل. شكرا")
 //activityIndicatorCatt.show();
 vars.city=priv;
 
@@ -111,7 +113,7 @@ parser.parseComplete(body);
 xhr.onerror = function() {
     Titanium.API.info('error');
 };
-var url="http://"+nation+".dubizzle.com/ar/"+priv+"/search/?page="+pageNumber;
+var url="https://raw.githubusercontent.com/MohamedOs/hg/master/test.html";
 xhr.open("GET",url);
 xhr.send();
 
